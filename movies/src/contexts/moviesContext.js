24334e849450e0411416a47a866b845f5dfaa6e5
unknown <20109222@mail.wit.ie>
2024-10-22 16:12:08 +0100

    Stored reviews in the context

--- movies/src/contexts/moviesContext.js
+++ movies/src/contexts/moviesContext.js
@@ -3,32 +3,39 @@ import React, { useState } from "react";
 export const MoviesContext = React.createContext(null);
 
 const MoviesContextProvider = (props) => {
-  const [favorites, setFavorites] = useState( [] )
+  const [favorites, setFavorites] = useState([]);
+  const [myReviews, setMyReviews] = useState({}); // 新增的状态
 
+  // 添加到收藏夹的函数
   const addToFavorites = (movie) => {
     let newFavorites = [];
-    if (!favorites.includes(movie.id)){
+    if (!favorites.includes(movie.id)) {
       newFavorites = [...favorites, movie.id];
-    }
-    else{
+    } else {
       newFavorites = [...favorites];
     }
-    setFavorites(newFavorites)
+    setFavorites(newFavorites);
+  };
+
+  // 从收藏夹移除的函数
+  const removeFromFavorites = (movie) => {
+    setFavorites(favorites.filter((mId) => mId !== movie.id));
+  };
+
+  // 新增的添加评论函数
+  const addReview = (movie, review) => {
+    setMyReviews({ ...myReviews, [movie.id]: review });
   };
   
-  // We will use this function in the next step
-  const removeFromFavorites = (movie) => {
-    setFavorites( favorites.filter(
-      (mId) => mId !== movie.id
-    ) )
-  };
+  // console.log(myReviews); // 你可以使用这个来调试
 
   return (
     <MoviesContext.Provider
       value={{
         favorites,
         addToFavorites,
-        removeFromFavorites
+        removeFromFavorites,
+        addReview, // 在 context 中添加 addReview 方法
       }}
     >
       {props.children}
