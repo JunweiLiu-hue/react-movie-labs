10b50f48a7cd90ce8c5ee4db550bf9d6ab6c3f25
unknown <20109222@mail.wit.ie>
2024-10-28 15:48:01 +0000

    Added Upcoming movies page.

--- movies/src/contexts/moviesContext.js
+++ movies/src/contexts/moviesContext.js
@@ -4,9 +4,8 @@ export const MoviesContext = React.createContext(null);
 
 const MoviesContextProvider = (props) => {
   const [favorites, setFavorites] = useState([]);
-  const [myReviews, setMyReviews] = useState({}); // 新增的状态
+  const [myReviews, setMyReviews] = useState({}); 
 
-  // 添加到收藏夹的函数
   const addToFavorites = (movie) => {
     let newFavorites = [];
     if (!favorites.includes(movie.id)) {
@@ -17,17 +16,15 @@ const MoviesContextProvider = (props) => {
     setFavorites(newFavorites);
   };
 
-  // 从收藏夹移除的函数
   const removeFromFavorites = (movie) => {
     setFavorites(favorites.filter((mId) => mId !== movie.id));
   };
 
-  // 新增的添加评论函数
   const addReview = (movie, review) => {
     setMyReviews({ ...myReviews, [movie.id]: review });
   };
   
-  // console.log(myReviews); // 你可以使用这个来调试
+
 
   return (
     <MoviesContext.Provider
@@ -35,7 +32,7 @@ const MoviesContextProvider = (props) => {
         favorites,
         addToFavorites,
         removeFromFavorites,
-        addReview, // 在 context 中添加 addReview 方法
+        addReview, 
       }}
     >
       {props.children}
